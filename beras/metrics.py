5123c6d1db4353855e02fcfe6007e17da396c7d6
ZahFay <alzahra_fayie@brown.edu>
2025-02-22 20:08:39 -0500

    metrics

--- beras/metrics.py
+++ beras/metrics.py
@@ -4,8 +4,10 @@ from beras.core import Callable
 
 
 class CategoricalAccuracy(Callable):
+    #from handout: probs represents the probability of each class as predicted by the model
+    #labels is a one hot encoded vector representing the true model class.
     def forward(self, probs, labels):
-        ## TODO: Compute and return the categorical accuracy of your model 
-        ## given the output probabilities and true labels. 
         ## HINT: Argmax + boolean mask via '=='
-        return NotImplementedError
+        arg_probs = np.argmax(probs, axis = 1)
+        arg_labels = np.argmax(labels, axis = 1)
+        return np.mean(arg_probs == arg_labels)
