f9f706cb69a0851b731550cffd29caf12dea4ebf
ZahFay <alzahra_fayie@brown.edu>
2025-02-24 20:36:43 -0500

    best

--- beras/optimizers.py
+++ beras/optimizers.py
@@ -27,7 +27,7 @@ class RMSProp:
 
             #Calculate new parameters
             denominator = np.sqrt(self.v[i]) + self.epsilon
-            # param.assign(param - (self.learning_rate / denominator) * grad) 
+            trainable_params[i].assign(param - (self.learning_rate / denominator) * grad) 
             # param.assign(param - (self.learning_rate / denominator) * grad)
 
 
