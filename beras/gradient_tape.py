0b90bc857f29b11ece3491317dc01d74df8903bf
ZahFay <alzahra_fayie@brown.edu>
2025-02-27 11:07:44 -0500

    test

--- beras/gradient_tape.py
+++ beras/gradient_tape.py
@@ -47,14 +47,14 @@ class GradientTape:
             layer = self.previous_layers[current_id] #gives the list of layers previous to the current layer, since it's linear we expect it to be a single one
             if layer != None:
                 inputs = layer.inputs
-
                 input_grad = layer.compose_input_gradients(grads[current_id])
-                queue.append(inputs)
                 grads[id(inputs)] = input_grad
+                queue.append(inputs)
 
                 weights = layer.weights
                 weight_grad = layer.compose_weight_gradients(grads[current_id])
                 grads[id(weights)] = weight_grad
+                queue.append(weights)
 
         output = []
         for source in sources:
